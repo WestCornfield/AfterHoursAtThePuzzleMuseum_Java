3e555d483e406a2b34d79b3d1fa68c7d0258aa21
John <John.Corwin@aa.com>
2020-07-26 23:13:35 -0500

    revise action parser to specifically check first word

--- src/main/com/afterhours/game/action/ActionParser.java
+++ src/main/com/afterhours/game/action/ActionParser.java
@@ -3,18 +3,27 @@ package com.afterhours.game.action;
 public class ActionParser {
     public Action parseAction(String input) {
         try {
+            String[] inputWords = splitInput(input);
+            Action actionToReturn = Action.GIBBERISH;
             for (Action action: Action.values()) {
                 for (String synonym: action.getSynonyms()) {
-                    if (input.toLowerCase().contains(synonym)) {
-                        return action;
+                    if (actionMatch(inputWords, synonym)) {
+                        actionToReturn = action;
                     }
                 }
             }
+            return actionToReturn;
         }
         catch (Exception e) {
             return Action.GIBBERISH;
         }
+    }
 
-        return Action.GIBBERISH;
+    private String[] splitInput(String input) {
+        return input.split(" ");
+    }
+
+    private boolean actionMatch(String[] inputWords, String synonym) {
+        return inputWords[0].toLowerCase().equals(synonym);
     }
 }
